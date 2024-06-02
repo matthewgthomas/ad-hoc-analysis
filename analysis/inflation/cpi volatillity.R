b8ffed9929e9d075a06f90b44ef3c3d163528ab6
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-06-02 11:07:22 +0100

    Explore volatility

--- analysis/inflation/cpi volatillity.R
+++ analysis/inflation/cpi volatillity.R
@@ -30,3 +30,64 @@ ppp <-
 ppp |>
   ggplot(aes(x = Year, y = `Internal purchasing power of the pound (based on RPI): 1982=100`)) +
   geom_line()
+
+# ---- Volatility in CPI components ----
+cpih_components_all <-
+  cpi_components |>
+  select(
+    Year,
+    starts_with("CPIH INDEX ")
+  )
+
+names(cpih_components_all) |> as_tibble() |> View()
+
+# Main components of CPIH
+cpih_components <-
+  cpih_components_all |>
+  select(Year, matches("CPIH INDEX [0-9][0-9]\\.[0-9] ")) |>
+  drop_na() |>
+
+  # Keep monthly data
+  filter(str_detect(Year, "[0-9]{4} [A-Z]{3}")) |>
+  mutate(Year = ym(Year)) |>
+
+  mutate(across(where(is.character), as.numeric))
+
+# Calculate coefficient of variation across components for each month
+cpih_volatility <-
+  cpih_components |>
+  rowwise(Year) |>
+  summarise(
+    mean = mean(c_across(where(is.double))),
+    sd = sd(c_across(where(is.double))),
+    cv = sd / mean
+  ) |>
+  ungroup()
+
+cpih_volatility |>
+  ggplot(aes(x = Year, y = cv)) +
+  geom_line()
+
+ggsave("analysis/inflation/CPIH volatility over time.png", width = 100, height = 100, units = "mm")
+
+# What are the most volatile components?
+cv <- list(
+  mean = ~mean(.x),
+  sd = ~sd(.x),
+  cv = ~sd(.x) / mean
+)
+
+cpih_volatility_components <-
+  cpih_components |>
+  select(-Year) |>
+  summarise(
+    across(where(is.double), ~sd(.x) / mean(.x))
+  ) |>
+  pivot_longer(cols = everything(), names_to = "CPIH component", values_to = "CV")
+
+cpih_volatility_components |>
+  ggplot(aes(x = reorder(`CPIH component`, CV, sum), y = CV)) +
+  geom_col() +
+  coord_flip()
+
+ggsave("analysis/inflation/CPIH volatility by component.png", width = 300, height = 175, units = "mm")
