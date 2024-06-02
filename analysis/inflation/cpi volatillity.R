db8e9a6580ae59efd81023d3a3e290c89ca75389
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-06-02 14:48:29 +0100

    Volatility of CPI subcomponents

--- analysis/inflation/cpi volatillity.R
+++ analysis/inflation/cpi volatillity.R
@@ -1,24 +1,25 @@
 # Explore how internal purchasing power of the pound has changed over time
 # Analyse variation/volatility in components of the consumer price index
 library(tidyverse)
+library(chron)
 
 # ---- Load consumer price index timeseries ----
 # Source: https://www.ons.gov.uk/economy/inflationandpriceindices/datasets/consumerpriceindices
 #Sys.setenv("VROOM_CONNECTION_SIZE" = 300000)
-cpi_components_raw <- read_csv("https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv")
+cpi_raw <- read_csv("https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv")
 #Sys.unsetenv("VROOM_CONNECTION_SIZE")
 
-# Check column names
-names(cpi_components_raw) |> as_tibble() |> View()
-
-cpi_components <-
-  cpi_components_raw |>
+cpi_raw <-
+  cpi_raw |>
   slice(-(1:6)) |>
   rename(Year = Title)
 
+# Check column names
+names(cpi_raw) |> as_tibble() |> View()
+
 # ---- Internal purchasing power of the pound ----
 ppp <-
-  cpi_components |>
+  cpi_raw |>
   select(
     Year,
     `Internal purchasing power of the pound (based on RPI): 1982=100`,
@@ -33,7 +34,7 @@ ppp |>
 
 # ---- Volatility in CPI components ----
 cpih_components_all <-
-  cpi_components |>
+  cpi_raw |>
   select(
     Year,
     starts_with("CPIH INDEX ")
@@ -98,3 +99,37 @@ ggsave("analysis/inflation/CPIH volatility by component.png", width = 300, heigh
 cpih_subcomponents <-
   cpih_components_all |>
   select(Year, matches("CPI INDEX [0-9][0-9]\\.[0-9]\\.[0-9]\\.[0-9] "))
+
+# ---- CPI components ----
+# Fetch CPI index components and subcomponents names
+# component_names <- sort(grep(pattern = "CPI INDEX \\d{2} ", names(cpi_raw), value = TRUE))
+component_names <- sort(grep(pattern = "CPI INDEX \\d{2}\\.\\d{1,2} ", names(cpi_raw), value = TRUE))
+subcomponent_names <- sort(grep(pattern = "CPI INDEX \\d{2}\\.\\d{1,2}\\.\\d{1,2} ", names(cpi_raw), value = TRUE))
+
+cpi_components <-
+  cpi_raw |>
+  select(Year, all_of(component_names)) |>
+
+  # Keep quarterly data from Q1 1988 onwards
+  filter(str_detect(Year, "[0-9]{4} Q\\d{1}")) |>
+  mutate(Year = yq(Year)) |>
+  filter(Year >= yq("1988 Q1")) |>
+
+  mutate(across(where(is.character), as.numeric))
+
+# Calculate coefficient of variation across components for each month
+cpi_volatility <-
+  cpi_components |>
+  rowwise(Year) |>
+  summarise(
+    mean = mean(c_across(where(is.double)), na.rm = TRUE),
+    sd = sd(c_across(where(is.double)), na.rm = TRUE),
+    cv = sd / mean
+  ) |>
+  ungroup()
+
+cpi_volatility |>
+  ggplot(aes(x = Year, y = cv)) +
+  geom_line()
+
+ggsave("analysis/inflation/CPI volatility over time.png", width = 100, height = 100, units = "mm")
