a2bc9e245a083f3b489a514b23665ae8ad0efa37
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-20 20:54:47 +0000

    Calculate inflation since an arbitrary year

--- analysis/inflation/cpi volatillity.R
+++ analysis/inflation/cpi volatillity.R
@@ -1,12 +1,14 @@
 # Explore how internal purchasing power of the pound has changed over time
 # Analyse variation/volatility in components of the consumer price index
 library(tidyverse)
+library(ggnewscale)
+library(ggiraph)
 
 # ---- Load consumer price index timeseries ----
 # Source: https://www.ons.gov.uk/economy/inflationandpriceindices/datasets/consumerpriceindices
-#Sys.setenv("VROOM_CONNECTION_SIZE" = 300000)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 300000)
 cpi_raw <- read_csv("https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv")
-#Sys.unsetenv("VROOM_CONNECTION_SIZE")
+Sys.unsetenv("VROOM_CONNECTION_SIZE")
 
 cpi_raw <-
   cpi_raw |>
@@ -92,9 +94,9 @@ ggsave("analysis/inflation/CPIH volatility by component.png", width = 300, heigh
 component_names <- sort(grep(pattern = "CPI INDEX \\d{2}\\.\\d{1,2} ", names(cpi_raw), value = TRUE))
 # subcomponent_names <- sort(grep(pattern = "CPI INDEX \\d{2}\\.\\d{1,2}\\.\\d{1,2} ", names(cpi_raw), value = TRUE))
 
-cpi_components <-
+cpi_index_and_components <-
   cpi_raw |>
-  select(Year, all_of(component_names)) |>
+  select(Year, `CPI INDEX 00: ALL ITEMS 2015=100`, all_of(component_names)) |>
 
   # Keep quarterly data from Q1 1988 onwards
   filter(str_detect(Year, "[0-9]{4} Q\\d{1}")) |>
@@ -102,6 +104,10 @@ cpi_components <-
   filter(Year >= yq("1988 Q1")) |>
 
   mutate(across(where(is.character), as.numeric))
+
+cpi_components <-
+  cpi_index_and_components |>
+  select(-`CPI INDEX 00: ALL ITEMS 2015=100`)
 
 # Calculate coefficient of variation across components for each month
 cpi_volatility <-
@@ -208,3 +214,105 @@ cpi_components_recessions |>
   )
 
 plotly::ggplotly()
+
+# ---- Inflation since an arbitrary year ----
+from_year <- 2000
+
+# Calculate overall inflation rate since `from_year`
+cpi_index_and_components |>
+  select()
+
+# Calculate percentage change for each component since `from_year`
+cpi_components_change <-
+  cpi_components |>
+  mutate(Year_only = year(Year)) |>
+  filter(Year_only >= from_year) |>
+  select(-Year_only) |>
+
+  mutate(across(starts_with("CPI"), ~ (.x - first(.x)) / first(.x)))
+
+cpi_components_change <-
+  cpi_components_change |>
+  pivot_longer(cols = -Year, names_to = "CPI component", values_to = "pct_change") |>
+
+  mutate(`CPI component` = str_remove(`CPI component`, "CPI INDEX [0-9]{2}\\.[0-9]{1} : ")) |>
+  mutate(`CPI component` = str_remove(`CPI component`, " 2015=100")) |>
+  mutate(`CPI component` = str_to_sentence(`CPI component`)) |>
+
+  # Fetch the most recent % change value for each CPI component: this will be used to colour the lines in the plot along a red or blue gradient
+  group_by(`CPI component`) |>
+  mutate(latest_value = last(pct_change)) |>
+  ungroup()
+
+from_year_text <- format(min(cpi_components_change$Year), "%B %Y")
+to_year_text <- format(max(cpi_components_change$Year), "%B %Y")
+
+# Which CPI components inflated or deflated since `from_year`
+# Inflated components will be coloured red; deflated will be blue
+inflated_components <-
+  cpi_components_change |>
+  filter(Year == max(Year) & pct_change > 0) |>
+  pull(`CPI component`)
+
+deflated_components <-
+  cpi_components_change |>
+  filter(Year == max(Year) & pct_change <= 0) |>
+  pull(`CPI component`)
+
+plt <-
+  cpi_components_change |>
+  filter(`CPI component` %in% inflated_components) |>
+
+  ggplot(aes(x = Year, y = pct_change, group = `CPI component`, data_id = `CPI component`)) +
+  geom_hline(yintercept = 0) +
+
+  geom_line_interactive(aes(
+    colour = latest_value,
+    tooltip = str_glue("By {format(Year, '%d %B %Y')}, the price of {tolower(`CPI component`)} increased by {scales::percent(pct_change, accuracy = 0.1)}")
+  )) +
+  scale_colour_gradient(low = "#ffe6e6", high = "#4d0000") +
+
+  new_scale_colour() +
+
+  geom_line_interactive(
+    data = cpi_components_change |> filter(`CPI component` %in% deflated_components),
+    aes(
+      colour = latest_value,
+      tooltip = str_glue("By {format(Year, '%d %B %Y')}, the price of {tolower(`CPI component`)} fell by {scales::percent(pct_change, accuracy = 0.1)}")
+    )
+  ) +
+  scale_colour_gradient(low = "#cce0ff", high = "#001f4d") +
+
+  scale_y_continuous(labels = scales::percent) +
+  theme_classic() +
+  theme(
+    plot.title.position = "plot",
+    legend.position = "none"
+  ) +
+  labs(
+    title = str_glue("Price changes in the UK from {from_year_text} to {to_year_text}"),
+    x = NULL,
+    y = NULL
+  )
+
+plt
+
+girafe(
+  ggobj = plt,
+  options = list(
+    opts_hover_inv(css = "opacity:0.1;"),
+    opts_hover(css = "stroke-width:2;"),
+
+    # opts_selection(type = "multiple", css = "fill:#FF851B;stroke:black;"),
+    opts_toolbar(saveaspng = FALSE, position = "topright", delay_mouseout = 10000),
+    opts_tooltip(
+      css = "background-color:black;color:white;padding:10px;border-radius:10px;box-shadow:10px 10px 10px rgba(0,0,0,0.3);font-family:Arial;font-size:12px;",
+      opacity = 0.9,
+      use_fill = TRUE
+    ),
+    opts_sizing(rescale = TRUE),
+    opts_zoom(max = 2)
+  )
+)
+
+# plotly::ggplotly(tooltip = "text")
