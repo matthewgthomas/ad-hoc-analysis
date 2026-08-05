11d5f8747ca14caf64bdfdc181c1d412618f748f
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2026-08-05 16:46:33 +0100

    Include hyper-prioritised countries in analysis

--- analysis/humanitarian-intensity/R/analyse.R
+++ analysis/humanitarian-intensity/R/analyse.R
@@ -1,17 +1,29 @@
 library(tidyverse)
 
-indices <- read_rds("analysis/humanitarian-intensity/output/humanitarian_indices_country.rds")
+indices <- read_rds(
+  "analysis/humanitarian-intensity/output/humanitarian_indices_country.rds"
+)
 
-brc_partners <- read_csv("analysis/humanitarian-intensity/data/BRC_partnerships.csv")
+brc_partners <- read_csv(
+  "analysis/humanitarian-intensity/data/BRC_partnerships.csv"
+)
+
+hyper_prioritised <- read_csv(
+  "analysis/humanitarian-intensity/data/hyper_prioritised_countries.csv"
+) |>
+  janitor::clean_names()
 
 indices <- left_join(indices, brc_partners) |>
-  mutate(partnership_category = replace_na(partnership_category, "No partnership")) |> 
+  left_join(hyper_prioritised) |>
+  mutate(
+    partnership_category = replace_na(partnership_category, "No partnership")
+  ) |>
   mutate(top_10_pct = top_10_count / indices_ranked_count)
 
-indices |> 
-  filter(partnership_category != "No partnership") |> 
+indices |>
+  filter(partnership_category != "No partnership") |>
   View()
 
-indices |> 
-  filter(partnership_category == "No partnership") |> 
+indices |>
+  filter(partnership_category == "No partnership") |>
   View()
